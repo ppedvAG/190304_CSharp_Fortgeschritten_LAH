c42c3089822feaa1c25650667e8bee4d4ead5f01
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-03-04 14:32:31 +0100

    Variante "schlecht"

--- Taschenrechner/Taschenrechner/Program.cs
+++ Taschenrechner/Taschenrechner/Program.cs
@@ -10,6 +10,27 @@ namespace Taschenrechner
     {
         static void Main(string[] args)
         {
+            // Variante "schlecht"
+
+            Console.WriteLine("Bitte geben Sie Ihre Formel ein:");
+            string eingabe = Console.ReadLine(); // "2 + 2"
+
+            string[] teile = eingabe.Split();
+
+            int zahl1 = Convert.ToInt32(teile[0]);
+            string op = teile[1];
+            int zahl2 = Convert.ToInt32(teile[2]);
+
+            int erg = 0;
+            if (op == "+")
+                erg = zahl1 + zahl2;
+            else if (op == "-")
+                erg = zahl1 - zahl2;
+
+            Console.WriteLine($"Das Ergebnis ist {erg}");
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
         }
     }
 }
