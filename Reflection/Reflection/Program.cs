933cf31967e14676b6bb34071d8e68d1439c1392
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-03-04 16:10:52 +0100

    Reflection

--- Reflection/Reflection/Program.cs
+++ Reflection/Reflection/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,28 @@ namespace Reflection
     {
         static void Main(string[] args)
         {
+            int zahl = 42;
+
+            Type intType = zahl.GetType();
+            Console.WriteLine(intType);
+
+            var instanz = Activator.CreateInstance(intType);
+
+            Console.WriteLine(instanz);
+            Console.WriteLine(instanz.GetType());
+
+            Console.WriteLine("------------------------");
+            Assembly taschenrechnerAssembly = Assembly.LoadFrom("MeineLib.dll");
+            Type taschenrechnerType = taschenrechnerAssembly.GetType("MeineLib.Taschenrechner"); // Variante 1: Wir kennen den Name
+
+            // taschenrechnerType.GetConstructor
+            var taschenrechnerInstanz = Activator.CreateInstance(taschenrechnerType);
+
+            MethodInfo methodenInfo = taschenrechnerType.GetRuntimeMethod("Add", new Type[] { typeof(int), typeof(int) });
+            var erg = methodenInfo.Invoke(taschenrechnerInstanz, new object[] { 12, 5 });
+
+            Console.WriteLine(erg);
+
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
         }
