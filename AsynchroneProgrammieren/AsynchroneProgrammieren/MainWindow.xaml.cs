f1fe769dc1c395868e7508fd813f99b241d6946e
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-03-05 13:29:10 +0100

    Eventbasiertes asynchrones Programmieren

--- AsynchroneProgrammieren/AsynchroneProgrammieren/MainWindow.xaml.cs
+++ AsynchroneProgrammieren/AsynchroneProgrammieren/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,38 @@ namespace AsynchroneProgrammieren
         public MainWindow()
         {
             InitializeComponent();
+            TaskIstFertigEvent += TaskIstFertig;
+        }
+
+        // Callback wenn der Task fertig ist
+        private void TaskIstFertig(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ende");
+        }
+
+        private event EventHandler TaskIstFertigEvent;
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Start");
+
+            MachEtwas();
+
+            labelWert.Content = "Es läuft ....";
+        }
+
+        private Task MachEtwas()
+        {
+            return Task.Run(() =>
+            {
+                for (int i = 0; i <= 100; i++)
+                {
+                    Thread.Sleep(100);
+                    Dispatcher.Invoke(() => progressBarWert.Value = i);
+                }
+                TaskIstFertigEvent?.Invoke(this, EventArgs.Empty);
+            });
         }
     }
 }
