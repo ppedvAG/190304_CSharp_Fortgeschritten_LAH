5424910f9cda8af33bb61e02cc6e64fe0d598c24
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-03-05 10:23:35 +0100

    Konto ohne lock

---
+++ Threads/Threads/Konto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    class Konto
+    {
+
+        private static int buchungsnummer = 0;
+        public Konto(decimal kontostand)
+        {
+            Kontostand = kontostand;
+        }
+        public decimal Kontostand { get; private set; }
+
+        public void Abheben(decimal betrag)
+        {
+            if(Kontostand >= betrag)
+            {
+                buchungsnummer++;
+                Console.WriteLine($"[{buchungsnummer}]Kontostand vor dem Abheben:\t\t{Kontostand}");
+                Console.WriteLine($"[{buchungsnummer}]Betrag zum Abheben:\t\t\t{betrag}");
+                Kontostand -= betrag;
+                Console.WriteLine($"[{buchungsnummer}]Kontostand nach dem Abheben:\t\t{Kontostand}");
+            }
+        }
+
+        public void Einzahlen(decimal betrag)
+        {
+            buchungsnummer++;
+            Console.WriteLine($"[{buchungsnummer}]Kontostand vor dem Einzahlen:\t\t{Kontostand}");
+            Console.WriteLine($"[{buchungsnummer}]Betrag zum Einzahlen:\t\t\t{betrag}");
+            Kontostand += betrag;
+            Console.WriteLine($"[{buchungsnummer}]Kontostand nach dem Einzahlen:\t\t{Kontostand}");
+        }
+
+    }
+}
--- Threads/Threads/Program.cs
+++ Threads/Threads/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Threads
@@ -10,10 +11,27 @@ namespace Threads
     {
         static void Main(string[] args)
         {
-
+            var konto = new Konto(1000);
+            for (int i = 0; i < 10; i++)
+                ThreadPool.QueueUserWorkItem(ZufälligesKontoUpdate, konto);
 
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
+        }
+
+        private static void ZufälligesKontoUpdate(object state)
+        {
+            var konto = (Konto)state;
+            var r = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                var betrag = r.Next(1, 100);
+                if (r.NextDouble() < 0.5)
+                    konto.Einzahlen(betrag);
+                else
+                    konto.Abheben(betrag);
+            }
+
         }
     }
 }
