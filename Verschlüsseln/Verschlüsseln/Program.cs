8116c3f695d65106bff4c8dd01e11bf5a94e8429
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-03-06 10:02:10 +0100

    Init Verschlüsseln

---
+++ Verschlüsseln/Verschlüsseln/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verschlüsseln
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
+        }
+    }
+}
